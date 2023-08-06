5ae1e7117b375164b04d5d8996309cbd544a59d9
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2023-08-07 03:15:03 +0530

    fixes delete functionality, makes style changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :contacts
-  # Defines the root path route ("/")
-  # root "articles#index"
+  delete '/contacts/:id', to: 'contacts#destroy', as: :delete_contact
 end
